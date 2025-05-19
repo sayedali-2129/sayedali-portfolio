207532bc19b19d8ac79bda8cd4a08f19d4a693e0
sayedali-2129 <sayedalipld@gmail.com>
2025-05-19 18:42:44 +0530

    error fixed

--- .history/next.config_20250516203921.ts
+++ .history/next.config_20250516203921.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
 };
 
 export default nextConfig;
